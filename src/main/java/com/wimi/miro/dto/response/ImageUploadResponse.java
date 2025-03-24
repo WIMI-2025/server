d6093b7de9ef803aa60cae8331b99bd0c5bd113c
givemethatsewon <nvpz1598@gmail.com>
2025-03-24 23:41:35 +0900

    직렬화 이슈 해결 - Response DTO에 누락된 Getter 추가

--- src/main/java/com/wimi/miro/dto/response/ImageUploadResponse.java
+++ src/main/java/com/wimi/miro/dto/response/ImageUploadResponse.java
@@ -2,8 +2,10 @@ package com.wimi.miro.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
 @AllArgsConstructor
 public class ImageUploadResponse {
     private String imageUrl;
